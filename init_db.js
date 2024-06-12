8317deb43f5faeb9bd7f2cf58f51b491295a68d6
khemi00 <safouane.khemiri@gmail.com>
2024-06-12 02:19:18 +0200

    Update init_db.js

--- init_db.js
+++ init_db.js
@@ -8,8 +8,12 @@ db.serialize(() => {
     password TEXT NOT NULL
   )`);
 
+  // Inserisci nuovi utenti qui
   db.run(`INSERT INTO user (username, password) VALUES (?, ?)`, ['admin', 'password']);
   db.run(`INSERT INTO user (username, password) VALUES (?, ?)`, ['user', 'pass123']);
+  db.run(`INSERT INTO user (username, password) VALUES (?, ?)`, ['new_user', 'new_pass']);
+  db.run(`INSERT INTO user (username, password) VALUES (?, ?)`, ['example_user', 'example_pass']);
 });
 
 db.close();
+console.log("Database users.db initialized successfully.");
