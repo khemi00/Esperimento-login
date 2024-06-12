570aed27f9454484ccef1e411cd7521a205aa3a1
khemi00 <safouane.khemiri@gmail.com>
2024-06-12 17:22:09 +0200

    Create init_db.js

---
+++ public/init_db.js
@@ -0,0 +1,13 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
+
+db.serialize(function() {
+  db.run("CREATE TABLE users (id INT, username TEXT, password TEXT)");
+
+  const stmt = db.prepare("INSERT INTO users VALUES (?, ?, ?)");
+  stmt.run(1, 'user1', 'password1');
+  stmt.run(2, 'user2', 'password2');
+  stmt.finalize();
+});
+
+module.exports = db;
